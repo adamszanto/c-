5971d56e5df3580b48373ad08c71e57c178d42b4
adamszanto <51482667+adamszanto@users.noreply.github.com>
2020-11-22 12:22:38 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,20 +3,30 @@
 
 int main()
 {
-    printf("Tartaly festese\n");
+    printf("Pick a number\n");
+    int number;
+    scanf("%d", &number);
 
-    double magas;
-    double atmero;
-
-    printf("Milyen magas?\n");
-    scanf("%lf", &magas);
-    printf("Mennyi az atmeroje?\n");
-    scanf("%lf", &atmero);
-
-    double sugar = atmero/2;
-    double doboz = (2*sugar*sugar*3.1416 + magas*2*sugar*3.1416) / 2;
-
-    printf("%f doboz festek kell.\n", doboz);
+    for(int i = 1; i <= number; i++)
+    {
+        if(i % 5 == 0 && i % 3 == 0)
+        {
+            printf("Fizz buzz\n");
+        }
+        else if(i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if(i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
 
     return 0;
+
 }
